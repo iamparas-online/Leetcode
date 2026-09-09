932f60a06056e542277b2016e88fe13be546d973
Paras <new043one@gmail.com>
2026-09-09 11:27:55 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (56.41%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long start = 1000;
+
+        while (start <= n) {
+            ans += n - start + 1;
+
+            if (start > n / 1000)
+                break;
+
+            start *= 1000;
+        }
+
+        return ans;
+    }
+}
