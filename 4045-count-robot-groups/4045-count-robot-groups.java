5b550f55a70f8e2a3b46535dd6a79f5e4ded7b27
Paras <new043one@gmail.com>
2026-09-20 18:47:07 +0530

    Time: 2 ms (100%), Space: 99.8 MB (68.32%) - LeetHub

---
+++ 4045-count-robot-groups/4045-count-robot-groups.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countGroups(int[] position, int[] speed, int distance) {
+        int n = position.length;
+        int group = 1;
+        int group_speed = speed[n-1]; //last robot speed 
+
+        for(int i=n-2;i>=0;i--){
+
+            if(position[i+1] - position[i] <= distance){
+                continue;
+            }
+            else if(speed[i] <= group_speed){
+                group++;
+                group_speed=speed[i];
+            }
+        }
+        return group;
+    }
+}
