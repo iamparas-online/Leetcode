2093741503875095dd65112a2dd940085110a1a2
Paras <new043one@gmail.com>
2026-09-20 18:01:10 +0530

    Time: 1 ms (99.95%), Space: 46.5 MB (57.72%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String r, String m) {
+        int [] count = new int [26];
+
+        for(int i=0;i<m.length();i++){
+            char c = m.charAt(i);
+            count[c - 'a']++;
+        }
+
+        for(int i=0;i<r.length();i++){
+            int c = r.charAt(i);
+            if(count[c - 'a'] == 0){
+                return false;
+            }
+            count[c - 'a']--;  
+        }
+        return true ;
+    }
+}
