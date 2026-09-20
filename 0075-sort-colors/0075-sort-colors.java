2110da0c4faf6e82202b49f9efd0578889e3e875
Paras <new043one@gmail.com>
2026-09-20 12:30:40 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.01%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int n=nums.length;
+        int i=0;    //0
+        int j=0;    //1
+        int k=n-1;  //2
+
+        while(j<=k){
+            if(nums[j]==1){
+                j++;
+            }
+            else if(nums[j]==2){
+
+                int temp = nums[j];
+                nums[j]=nums[k];
+                nums[k]=temp;
+
+                k--;
+            }
+            else{
+
+                int temp = nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+
+                i++;
+                j++;
+            }
+        }
+    }
+}
