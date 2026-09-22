702dc0da76a184439f410e72c4d085f5a39deead
Paras <new043one@gmail.com>
2026-09-23 00:18:30 +0530

    Time: 0 ms (100%), Space: 42.8 MB (86.59%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -2,11 +2,11 @@ class Solution {
     public int wiggleMaxLength(int[] nums) {
         int up=1;
         int down=1;
-      /*
+      
         if(nums.length<2){
             return nums.length;
         }
-    */
+    
         for(int i=1; i<nums.length; i++){
             
             if(nums[i]>nums[i-1]){
