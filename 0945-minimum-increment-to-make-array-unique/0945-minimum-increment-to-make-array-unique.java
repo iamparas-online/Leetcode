7dc49ff05411514a1ca463d61faa96e73b108eff
Paras <new043one@gmail.com>
2026-09-23 16:17:55 +0530

    Time: 44 ms (36.64%), Space: 79.9 MB (73.83%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int ans=0;
+
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]<=nums[i-1]){
+
+                int x = nums[i-1]+1;
+                ans += x-nums[i];
+                nums[i] = x;
+            }
+        }
+        return ans;
+    }
+}
